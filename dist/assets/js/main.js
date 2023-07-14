bac2564b131c9f2fe6b8f07c4ada14d99481dca2
Andrew <andrewcodereview@gmail.com>
2023-07-14 03:00:58 +0300

    first commit

---
+++ dist/assets/js/main.js
@@ -0,0 +1,52 @@
+const counters = document.querySelectorAll('.section__list-item-valuecount')
+
+
+counters.forEach(function(counter) {
+	counter.addEventListener('click', function(e) {
+		const operator = e.target.dataset.action
+		const number = counter.querySelector('.section__list-item-number')
+		const sectionList = counter.closest('.section__list')
+		const price = sectionList.querySelector('.section__list-item-price')
+
+		if (operator == 'plus' && number.innerText < 30) {
+			number.innerText = ++number.innerText
+
+			if (e.target.hasAttribute('guest')) {
+				price.innerText = parseInt(price.innerText) + 87
+			}
+			
+		}
+		
+		if (operator == 'minus' && number.innerText > 1) {
+			number.innerText = --number.innerText
+
+			if (e.target.hasAttribute('guest')) {
+				price.innerText = parseInt(price.innerText) - 87
+			}
+		}
+	})
+})
+
+
+const productPoints = document.querySelectorAll('.shop__product-point')
+const shop = document.querySelector('.shop')
+
+productPoints.forEach(function(productPoint) {
+	productPoint.addEventListener('click', function() {
+		productPoint.classList.toggle('shop__product-point--active')
+
+		productPoint.querySelector('.shop__product-point-content').addEventListener('click', function(e) {
+			e.stopPropagation()
+		})
+	})
+})
+
+shop.addEventListener('click', function(e) {
+	productPoints.forEach(function(productPoint) {
+			productPoint.classList.remove('shop__product-point--active')
+
+			productPoint.addEventListener('click', function(e) {
+				e.stopPropagation()
+			})
+		})
+	})
---
+++ dist/assets/js/myslick.js
@@ -0,0 +1,58 @@
+$(function(){
+	$('.header__slider-dots').slick({
+		dots: false,
+		slidesToShow: 4,
+ 		slidesToScroll: 4,
+		asNavFor: '.header__slider',
+		focusOnSelect: true,
+	});
+
+	$('.header__slider').slick({
+		dots: false,
+		arrows: true,
+		prevArrow: '<img class="slick-prev" src="assets/images/common/arrow-left.svg">',
+		nextArrow: '<img class="slick-next" src="assets/images/common/arrow-right.svg">',
+		infinite: true,
+		speed: 300,
+		fade: true,
+		asNavFor: '.header__slider-dots',
+	  });
+
+	$('.surf__map-points-slider').slick({
+		arrows: false,
+		slidesToShow: 8,
+ 		slidesToScroll: 1,
+		asNavFor: '.surf__slider',
+		focusOnSelect: true,
+	});
+
+	$('.surf__slider').slick({
+		arrows: true,
+		prevArrow: '<img class="slick-prev surf__slick-prev" src="assets/images/common/gray-arrow-left.svg">',
+		nextArrow: '<img class="slick-next surf__slick-next" src="assets/images/common/gray-arrow-right.svg">',
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		asNavFor: '.surf__map-points-slider',
+	});
+
+	$('.section__slider').slick({
+		fade: true,
+		arrows: true,
+		prevArrow: '<img class="slick-prev surf__slick-prev" src="assets/images/common/gray-arrow-left.svg">',
+		nextArrow: '<img class="slick-next surf__slick-next" src="assets/images/common/gray-arrow-right.svg">',
+		infinite: true,
+		slidesToShow: 1,
+ 		slidesToScroll: 1,
+	});
+
+	$('.shop__slider').slick({
+		fade: true,
+		arrows: true,
+		prevArrow: '<img class="slick-prev surf__slick-prev" src="assets/images/common/gray-arrow-left.svg">',
+		nextArrow: '<img class="slick-next surf__slick-next" src="assets/images/common/gray-arrow-right.svg">',
+		infinite: true,
+		slidesToShow: 1,
+ 		slidesToScroll: 1,
+	});
+})
